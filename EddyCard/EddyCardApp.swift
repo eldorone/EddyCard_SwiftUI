c6ca622a70a8f44119d14a530223bb0a24a1f4b9
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-07 20:35:23 +0900

    Initial Commit

---
+++ EddyCard/EddyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EddyCardApp.swift
+//  EddyCard
+//
+//  Created by Eldor Alikuvatov on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct EddyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
