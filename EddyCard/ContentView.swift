d15637d9cb5a2519c774aca71b6ce653bb667519
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-07 21:42:14 +0900

    Update ContentView.swift

--- EddyCard/ContentView.swift
+++ EddyCard/ContentView.swift
@@ -30,8 +30,8 @@ struct ContentView: View {
                 
                 Divider()
                 RoundedRectangle(cornerRadius: 30)
-                    .frame(width: 390, height: 40)
-                    .foregroundColor(.white)
+                    .fill(Color.white)
+                    .frame(height: 40)
                     .overlay(Text("+82 10 1122 3344")
                         .bold()
                         .foregroundColor(.black))
